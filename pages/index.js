b7721fb3a2ea0f50f2bffafc6579929ab91ee73c
Mike <slavik433@gmail.com>
2022-08-11 14:08:53 +0300

    learning getStaticProps, getServerSideProps, getStaticPaths

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import MeetupList from '../components/meetups/MeetupList';
+import {getStaticPaths} from "./[meetupId]";
 
 const DUMMY_MEETUPS = [
   {
@@ -19,8 +20,31 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
+}
+
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   // fetch data from an API
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS
+//     }
+//   };
+// }
+
+export async function getStaticProps() {
+  // fetch data from an API
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS
+    },
+    revalidate: 1
+  }; 
 }
 
 export default HomePage;
